762872d921a97c559db1350f14a810ff4748c676
Simone Conti <contisimone4@gmail.com>
2025-01-04 17:41:28 +0100

    Update snh_webproj_novels.sql
    Fixed wrong type for date in SQL db

--- db/snh_webproj_novels.sql
+++ db/snh_webproj_novels.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Creato il: Dic 12, 2024 alle 11:11
--- Versione del server: 10.4.32-MariaDB
--- Versione PHP: 8.2.12
+-- Host: localhost
+-- Generation Time: Jan 04, 2025 at 05:34 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -26,9 +26,8 @@ USE `snh_webproj_novels`;
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `log_attempts`
+-- Table structure for table `log_attempts`
 --
-
 DROP TABLE IF EXISTS `log_attempts`;
 CREATE TABLE `log_attempts` (
   `ID` int(11) NOT NULL,
@@ -38,7 +37,7 @@ CREATE TABLE `log_attempts` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dump dei dati per la tabella `log_attempts`
+-- Dumping data for table `log_attempts`
 --
 
 INSERT INTO `log_attempts` (`ID`, `IP_ADDR`, `TIME`, `N_ATTEMPTS`) VALUES
@@ -53,15 +52,14 @@ INSERT INTO `log_attempts` (`ID`, `IP_ADDR`, `TIME`, `N_ATTEMPTS`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `long_novels`
+-- Table structure for table `long_novels`
 --
-
 DROP TABLE IF EXISTS `long_novels`;
 CREATE TABLE `long_novels` (
   `ID` int(11) NOT NULL COMMENT 'ID univoco del record',
   `ID_U` int(11) NOT NULL COMMENT 'ID dell''utente che ha creato la novel',
   `PREMIUM` int(11) NOT NULL COMMENT 'Premium (1) o no (0)',
-  `DATE` int(11) NOT NULL COMMENT 'Data di creatione (timestamp)',
+  `DATE` datetime NOT NULL COMMENT 'Data di creatione (timestamp)',
   `TITLE` varchar(255) NOT NULL COMMENT 'Titolo della novel',
   `FILENAME` varchar(255) NOT NULL COMMENT 'Nome del fil PDF salvato sul server'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -69,9 +67,8 @@ CREATE TABLE `long_novels` (
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `psw_resets`
+-- Table structure for table `psw_resets`
 --
-
 DROP TABLE IF EXISTS `psw_resets`;
 CREATE TABLE `psw_resets` (
   `ID` int(11) NOT NULL,
@@ -81,7 +78,7 @@ CREATE TABLE `psw_resets` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dump dei dati per la tabella `psw_resets`
+-- Dumping data for table `psw_resets`
 --
 
 INSERT INTO `psw_resets` (`ID`, `ID_U`, `TOKEN`, `EXPIRE`) VALUES
@@ -93,9 +90,8 @@ INSERT INTO `psw_resets` (`ID`, `ID_U`, `TOKEN`, `EXPIRE`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `remember_me_tokens`
+-- Table structure for table `remember_me_tokens`
 --
-
 DROP TABLE IF EXISTS `remember_me_tokens`;
 CREATE TABLE `remember_me_tokens` (
   `ID` int(11) NOT NULL,
@@ -105,42 +101,36 @@ CREATE TABLE `remember_me_tokens` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dump dei dati per la tabella `remember_me_tokens`
+-- Dumping data for table `remember_me_tokens`
 --
 
 INSERT INTO `remember_me_tokens` (`ID`, `ID_U`, `TOKEN`, `EXPIRE`) VALUES
-(1, 17, '62aeafc24d20a4ebe5de7f23e47a7b39956d26f16cf5659dddd1ec04192cd758', 1735561544);
+(1, 17, '62aeafc24d20a4ebe5de7f23e47a7b39956d26f16cf5659dddd1ec04192cd758', 1735561544),
+(2, 23, '99f46e88fffa4d9db7e82e544b5502bbd160713916c84c08420726c62a25a121', 1738595115),
+(3, 23, '1849c366df9f0113b7756ca96444b4e2bf6e7c7a05ca9cb04a8579f413c398af', 1738596036),
+(4, 23, 'cdb7bba57e270ec96467d3f09858c6cacee632f8a1fb4fd701b72b4bee643e1b', 1738596609),
+(5, 23, '4ce69fd9f821f05a393c82b71278081eda6fa27e125aa59c1d06f04374b9f88d', 1738600236);
 
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `short_novels`
+-- Table structure for table `short_novels`
 --
-
 DROP TABLE IF EXISTS `short_novels`;
 CREATE TABLE `short_novels` (
   `ID` int(11) NOT NULL,
   `ID_U` int(11) NOT NULL,
   `PREMIUM` int(11) NOT NULL,
-  `DATE` int(11) NOT NULL,
+  `DATE` datetime NOT NULL,
   `TITLE` varchar(255) NOT NULL,
   `CONTENT` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dump dei dati per la tabella `short_novels`
---
-
-INSERT INTO `short_novels` (`ID`, `ID_U`, `PREMIUM`, `DATE`, `TITLE`, `CONTENT`) VALUES
-(4, 6, 1, 1732785664, 'Prova di gabri', 'Alert(&amp;quot;&amp;quot;)'),
-(7, 22, 1, 2147483647, 'Il Viaggio Magico di Luna', 'alert(&amp;quot;ciao&amp;quot;)');
-
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `users`
+-- Table structure for table `users`
 --
-
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `ID` int(11) NOT NULL,
@@ -153,92 +143,93 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dump dei dati per la tabella `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`ID`, `PREMIUM`, `USERNAME`, `NAME`, `SURNAME`, `EMAIL`, `PASSWORD`) VALUES
 (16, 1, 'admin', 'admin', 'admin', 'test@test.com', '$2y$10$7N33TFqkxtQmSH7nU8Nf6OmME0gLGvUr/B0zPzK.M4RXRvSdRzxka'),
-(22, 1, 'gabri', 'Gabriele', 'Galli', 'gabg3000@gmail.com', '$2y$10$WlTUKpeGXXyCuvUlKVWjp.EAY4nGzYeKvgBKjF3WnZZko/PGvVHue');
+(22, 1, 'gabri', 'Gabriele', 'Galli', 'gabg3000@gmail.com', '$2y$10$WlTUKpeGXXyCuvUlKVWjp.EAY4nGzYeKvgBKjF3WnZZko/PGvVHue'),
+(23, 1, 'simone', 'Simone', 'Conti', 'contisimone4@gmail.com', '$2y$10$t0wIxuJs89e2JiA1O6Gx1.0edMF40a31rDO6d3a0A015w8aGtfIqC');
 
 --
--- Indici per le tabelle scaricate
+-- Indexes for dumped tables
 --
 
 --
--- Indici per le tabelle `log_attempts`
+-- Indexes for table `log_attempts`
 --
 ALTER TABLE `log_attempts`
   ADD PRIMARY KEY (`ID`);
 
 --
--- Indici per le tabelle `long_novels`
+-- Indexes for table `long_novels`
 --
 ALTER TABLE `long_novels`
   ADD PRIMARY KEY (`ID`);
 
 --
--- Indici per le tabelle `psw_resets`
+-- Indexes for table `psw_resets`
 --
 ALTER TABLE `psw_resets`
   ADD PRIMARY KEY (`ID`);
 
 --
--- Indici per le tabelle `remember_me_tokens`
+-- Indexes for table `remember_me_tokens`
 --
 ALTER TABLE `remember_me_tokens`
   ADD PRIMARY KEY (`ID`);
 
 --
--- Indici per le tabelle `short_novels`
+-- Indexes for table `short_novels`
 --
 ALTER TABLE `short_novels`
   ADD PRIMARY KEY (`ID`);
 
 --
--- Indici per le tabelle `users`
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`ID`);
 
 --
--- AUTO_INCREMENT per le tabelle scaricate
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT per la tabella `log_attempts`
+-- AUTO_INCREMENT for table `log_attempts`
 --
 ALTER TABLE `log_attempts`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT per la tabella `long_novels`
+-- AUTO_INCREMENT for table `long_novels`
 --
 ALTER TABLE `long_novels`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID univoco del record', AUTO_INCREMENT=12;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID univoco del record', AUTO_INCREMENT=13;
 
 --
--- AUTO_INCREMENT per la tabella `psw_resets`
+-- AUTO_INCREMENT for table `psw_resets`
 --
 ALTER TABLE `psw_resets`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
--- AUTO_INCREMENT per la tabella `remember_me_tokens`
+-- AUTO_INCREMENT for table `remember_me_tokens`
 --
 ALTER TABLE `remember_me_tokens`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT per la tabella `short_novels`
+-- AUTO_INCREMENT for table `short_novels`
 --
 ALTER TABLE `short_novels`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
--- AUTO_INCREMENT per la tabella `users`
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
